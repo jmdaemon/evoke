9ec59f4da6fa7820c66159ec01e872d8716ae3ec
Joseph Diza <josephm.diza@gmail.com>
2022-04-26 12:49:41 -0700

    Stub implementation code to open wordnet database

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::fs::read_dir;
+
+/// Wordnet database dictionary
+#[derive(Default, Debug)]
+pub struct Wordnet {
+}
+
+/// Open a wordnet database directory
+pub fn open(path: &str) -> Wordnet {
+    // Stub the implementation for now
+    let files = std::fs::read_dir(path).unwrap();
+    for file in files {
+        let entry = file.unwrap();
+    }
+    Wordnet {}
+}
+
 fn main() {
     println!("Hello, world!");
 }
